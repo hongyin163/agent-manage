e88c156f18112c9fe6c0143119bfbf14aa0bc908
hongyin163 <hongyin163@163.com>
2023-06-21 19:07:24 +0800

    add callback

--- src/components/LoginBtn.tsx
+++ src/components/LoginBtn.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useEffect } from "react";
 import ReactGoogleButton from "react-google-button";
+import { useSearchParams } from "next/navigation";
 
 export default function Component() {
+  const searchParams = useSearchParams();
+
+  const callbackUrl = searchParams?.get("callbackUrl");
   const { data: session, status } = useSession();
   console.log(session);
+
+  useEffect(() => {
+    if (session && callbackUrl) {
+      window.location.href = callbackUrl;
+    }
+  }, [session, callbackUrl]);
+
   if (status == "loading") {
     return <div>Loading</div>;
   }
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,11 @@ import { NextRequest } from "next/server";
 import { withAuth } from "next-auth/middleware"
 
 export default withAuth(function middleware(req) {
-  console.log(req.nextauth)
+  console.log(req)
+  if (req.nextUrl.pathname === '/auth/login/' && req.nextauth.token) {
+    return NextResponse.redirect(new URL('/home', req.url))
+
+  }
 }, {
   pages: {
     signIn: "/auth/login",
@@ -12,10 +16,11 @@ export default withAuth(function middleware(req) {
     // verifyRequest: '/auth/verify-request', // (used for check email message)
     // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
   },
+
   callbacks: {
     authorized: ({ token }) => {
       return !!token;
-    }
+    },
   }
 })
 // See "Matching Paths" below to learn more
